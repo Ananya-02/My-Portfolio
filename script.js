a8134c53c359a8c14edc40dd5d0db85bfa0f0cb3
Ananya Anindita <aninditaananya123@gmail.com>
2023-06-25 11:49:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents= document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+
+    }
+
+
+
+const msg = document.getElementById("msg");
+ form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+
+    msg.InnerHTML = "Message sent successfully"
+    setTimeout(function(){
+     msg.InnerHTML = ""
+    },10000)
+    form.reset()
+})
+ .catch(error => console.error('Error!', error.message))
+})
